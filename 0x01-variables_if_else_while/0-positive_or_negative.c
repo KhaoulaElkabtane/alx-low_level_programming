c996599535de87be1937524ff4018ef706e462b9
KhaoulaElkabtane <elkabtanekhaoula@gmail.com>
2023-06-16 10:00:17 -0700

    Dragon

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
-#include <stdlio.h>
+#include <stdio.h>
+
 #include <time.h>
+
 /* more headers goes there */
 /* betty style doc for function main goes there */
 int main(void)
