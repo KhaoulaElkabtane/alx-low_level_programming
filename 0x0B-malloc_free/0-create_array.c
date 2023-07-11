dc4151bad9000d33604eb76ab503146618599369
elkabtanekhaoula <elkabtanekhoula@gmail.com>
2023-07-11 16:01:46 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,18 +5,23 @@
 
 char *create_array(unsigned int size, char c)
 {
-        char *tab;
+	char *tab;
 	int i;
-	if(size == 0){
-		return NULL;
+	if (size == 0)
+	{
+		return (NULL);
 	}
-	tab=(char*)malloc(size*sizeof(char));
-	if(tab==NULL){
-		return NULL;
-	}else{
-		for(i=0 ;i<(int)size ;i++){
-			tab[i]=c;
+	tab = (char *) malloc(size * sizeof(char));
+	if (tab == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0 ; i < (int)size ; i++)
+		{
+			tab[i] = c;
 		}
-		return tab;
+		return (tab);
 	}
 }
