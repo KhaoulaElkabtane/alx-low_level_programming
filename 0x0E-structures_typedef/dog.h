7f52c4246e7ee6dd19722819023f2c4be8e22ea5
elkabtanekhaoula <elkabtanekhoula@gmail.com>
2023-07-17 16:51:43 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
-* Define a new type struct dog with the following elements:
-* @name, type = char *
-* @age, type = float
-* @owner, type = char *
-*
-* Description: just a long dog struct in a big kitty world
-*/
+ /**
+ * Define a new type struct dog with the following elements:
+ * @name, type = char *
+ * @age, type = float
+ * @owner, type = char *
+ *
+ * Description: just a long dog struct in a big kitty world
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
-}
+};
 
 #endif
 
